33b35a3882c212a2992a1ba78f685906d16449b8
Noriyuki Ishida <noripi@retty.me>
2017-11-16 00:31:34 +0900

    add outlet example

--- src/main/kotlin/ViewController.kt
+++ src/main/kotlin/ViewController.kt
@@ -4,7 +4,16 @@ import platform.UIKit.*
 
 @ExportObjCClass
 class ViewController(aDecoder: NSCoder) : UIViewController(aDecoder) {
+    @ObjCOutlet
+    private lateinit var label: UILabel
+
     override fun initWithCoder(aDecoder: NSCoder) = this.initBy(ViewController(aDecoder))
 
     override fun debugDescription() = "ViewController"
+
+    override fun viewDidLoad() {
+        super.viewDidLoad()
+
+        this.label.text = "TEST"
+    }
 }
