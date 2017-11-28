515aacc90b233931d546e08cd825597a12e35a5c
Noriyuki Ishida <noripi@retty.me>
2017-11-28 20:01:23 +0900

    remove unneeded typealias

--- src/main/kotlin/ListViewController.kt
+++ src/main/kotlin/ListViewController.kt
@@ -1,6 +1,7 @@
 import kotlinx.cinterop.*
 import platform.Foundation.*
 import platform.UIKit.*
+import platform.CoreGraphics.*
 
 @ExportObjCClass
 @Suppress("CONFLICTING_OVERLOADS")
--- src/main/kotlin/typealiases.kt
+++ src/main/kotlin/typealiases.kt
@@ -1,2 +1,1 @@
 typealias NSInteger = Long
-typealias CGFloat = Double
