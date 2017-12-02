8f5072a08619dc7502f2da6aad23a6a2372167cc
Noriyuki Ishida <noripi@retty.me>
2017-12-02 19:24:58 +0900

    rebuit on 0.4

--- src/main/kotlin/extensions/AnyExtensions.kt
+++ src/main/kotlin/extensions/AnyExtensions.kt
@@ -1,7 +1,7 @@
 package extensions
 
 /**
- * It should be defined in Kotlin1.1 but somehow it cannot be found in Kotlin/Native 0.3;
+ * It should be defined in Kotlin1.1 but somehow it cannot be found in Kotlin/Native 0.4;
  * Delete this function when it is defined
  */
 inline fun <T> T.takeIf(predicate: (T) -> Boolean): T? = if (predicate(this)) this else null
